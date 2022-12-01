ac81f935d6b09d5d75646f973631fd76124e1398
Matthias Mahrhofer <matthias.mahrhofer@audi.de>
2022-12-01 21:46:44 +0100

    1a - some refactoring

--- 1/main.go
+++ 1/main.go
@@ -7,6 +7,35 @@ import (
 	"strconv"
 )
 
+type elve struct {
+	calories int
+}
+
+func newElve() *elve {
+	return &elve{calories: 0}
+}
+
+func (e *elve) takePackage(call string) (packagesComplete bool, err error) {
+	if call == "" {
+		return true, nil
+	} else {
+		newPackage, err := strconv.Atoi(call)
+		if err != nil {
+			return false, err
+		}
+		e.calories += newPackage
+		return false, nil
+	}
+}
+
+func (e elve) returnRicher(other *elve) *elve {
+	if e.calories > other.calories {
+		return &e
+	} else {
+		return other
+	}
+}
+
 func main() {
 
 	f, err := os.Open("./1/input.txt")
@@ -22,24 +51,18 @@ func main() {
 
 	s := bufio.NewScanner(f)
 	s.Split(bufio.ScanLines)
-	richElve := 0
-	elvePackage := 0
+	richElve := newElve()
+	currentElve := newElve()
 	for s.Scan() {
-		caloriesCall := s.Text()
-		switch caloriesCall {
-		case "":
-			if elvePackage > richElve {
-				richElve = elvePackage
-			}
-			elvePackage = 0
-		default:
-			calories, err := strconv.Atoi(caloriesCall)
-			if err != nil {
-				panic(err)
-			}
-			elvePackage += calories
+		packageComplete, err := currentElve.takePackage(s.Text())
+		if err != nil {
+			panic(err)
+		}
+		if packageComplete {
+			richElve = richElve.returnRicher(currentElve)
+			currentElve = newElve()
 		}
 	}
-	log.Printf("%v", richElve)
+	log.Printf("%v", richElve.calories)
 
 }
