82a23395ff850d0bd4c74c599b743f81731d5348
Matthias Mahrhofer <matthias.mahrhofer@audi.de>
2022-12-04 22:17:49 +0100

    3b - complete

--- 3/main.go
+++ 3/main.go
@@ -18,13 +18,24 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	log.Printf("Total scores: %v", result)
+	log.Printf("3a Total scores: %v", result)
+
+	s2, err := internal.LineScannerFromFile("./3/input.txt")
+	defer s2.Close()
+	if err != nil {
+		log.Panic(err)
+	}
+	result, err = calculateGroupScore(s2, false)
+	if err != nil {
+		panic(err)
+	}
+	log.Printf("3b Group scores: %v", result)
 }
 
 func calculateScore(s *internal.LineScanner, debug bool) (sum int, err error) {
 	for s.Scan() {
 		line := s.Text()
-		char, err := locateItem(line)
+		char, err := locateDoubleItem(line)
 		if err != nil {
 			panic(err)
 		}
@@ -39,8 +50,35 @@ func calculateScore(s *internal.LineScanner, debug bool) (sum int, err error) {
 	}
 	return
 }
+func calculateGroupScore(s *internal.LineScanner, debug bool) (sum int, err error) {
+	for s.Scan() {
+		var lines [3]string
+		lines[0] = s.Text()
+		if !s.Scan() {
+			return 0, errors.New("unexpected end of list")
+		}
+		lines[1] = s.Text()
+		if !s.Scan() {
+			return 0, errors.New("unexpected end of list")
+		}
+		lines[2] = s.Text()
+		char, err := locateGroupItem(lines)
+		if err != nil {
+			panic(err)
+		}
+		score, err := calculateRuneScore(char)
+		if err != nil {
+			panic(err)
+		}
+		sum += score
+		if debug {
+			log.Printf("%c %v\t%v", char, score, lines)
+		}
+	}
+	return
+}
 
-func locateItem(line string) (rune, error) {
+func locateDoubleItem(line string) (rune, error) {
 	for _, c := range line[0 : len(line)/2] {
 		if strings.Count(line[len(line)/2:], string(c)) > 0 {
 			return c, nil
@@ -57,4 +95,36 @@ func calculateRuneScore(c rune) (priority int, err error) {
 		return priority - 64 + 26, nil
 	}
 	return 0, errors.New("")
+}
+func locateGroupItem(lines [3]string) (r rune, err error) {
+	var sb strings.Builder
+	for _, line := range lines {
+		cleanLine, err := eliminateDuplicate(line)
+		if err != nil {
+			panic(err)
+		}
+		sb.WriteString(cleanLine)
+	}
+	runes := make(map[rune]int)
+	for _, r := range sb.String() {
+		occurence := runes[r]
+		if occurence == 2 {
+			return r, nil
+		}
+		runes[r] = occurence + 1
+	}
+	return 0, errors.New("Missing common item")
+}
+
+func eliminateDuplicate(line string) (string, error) {
+	m := map[rune]bool{}
+	for _, r := range line {
+		m[r] = true
+	}
+	var sb strings.Builder
+	sb.Grow(len(m))
+	for r := range m {
+		sb.WriteRune(r)
+	}
+	return sb.String(), nil
 }
--- 3/main_test.go
+++ 3/main_test.go
@@ -18,9 +18,10 @@ CrZsJsPPZsGzwwsLwLmpwMDw`)
 	sum, _ := calculateScore(ls, true)
 	assert.Equal(t, 157, sum)
 }
+
 func Test_LocateItem(t *testing.T) {
 	input := "vJrwpWtwJgWrhcsFMMfFFhFp"
-	doubleItem, err := locateItem(input)
+	doubleItem, err := locateDoubleItem(input)
 	assert.NoError(t, err)
 	assert.Equal(t, 'p', doubleItem)
 }
@@ -48,4 +49,29 @@ func Test_calculateRuneScore(t *testing.T) {
 			assert.Equal(t, test.expect, score)
 		})
 	}
+}
+func Test_calculateGroupScore(t *testing.T) {
+	tests := []struct {
+		name   string
+		input  string
+		expect int
+	}{
+		{"first group", "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg", 18},
+		{"second group", "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw", 52},
+		{"both groups", "vJrwpWtwJgWrhcsFMMfFFhFp\njqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\nPmmdzqPrVvPwwTWBwg\nwMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\nttgJtRGJQctTZtZT\nCrZsJsPPZsGzwwsLwLmpwMDw", 70},
+	}
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ls, _ := internal.LineScannerFromString(test.input)
+			score, err := calculateGroupScore(ls, true)
+			assert.NoError(t, err)
+			assert.Equal(t, test.expect, score)
+		})
+	}
+}
+
+func Test_eliminateDuplicate(t *testing.T) {
+	got, err := eliminateDuplicate("abcbd")
+	assert.NoError(t, err)
+	assert.Equalf(t, "acbd", got, "eliminateDuplicate(abcbd)")
 }
